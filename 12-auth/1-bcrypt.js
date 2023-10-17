b8d00985c46294466ee1a3942a903827f817eb8e
dev-dong <dho0647@naver.com>
2023-10-18 00:49:14 +0900

    final

--- 12-auth/1-bcrypt.js
+++ 12-auth/1-bcrypt.js
@@ -6,3 +6,6 @@ console.log(`password: ${password}, hashed: ${hashed}`);
 
 const result = bcrypt.compareSync('abcd1234', hashed); // 비밀번호가 일치하는지 확인
 console.log(result);
+
+const hashSync = bcrypt.hashSync('12345', 12);
+console.log(hashSync);
